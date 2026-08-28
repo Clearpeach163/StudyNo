12f8213695d038ae39094acace9f0f8b632e4823
Clearpeach163 <davidmakje@icloud.com>
2026-08-28 13:28:15 +0200

    update planner to liquid glass

--- Update.js
+++ Update.js
@@ -4,7 +4,7 @@ const whatsnew = `
    
 
 `;
-const update = "28.8.26.v4";
+const update = "28.8.26.v6";
 if (typeof update !== "undefined") { //zorgt dat als update er niet is dat dan niet heel de website crashet
   document.getElementById("update").innerText = update;
 }
