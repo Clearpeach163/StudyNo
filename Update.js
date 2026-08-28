3d81b41c98914616cbe0e090a2a2276da8c434d8
Clearpeach163 <davidmakje@icloud.com>
2026-08-28 11:24:51 +0200

    adding planner

--- Update.js
+++ Update.js
@@ -1,15 +1,10 @@
 const whatsnew = `
-  What is new in the new version,
-    -new update system
-    -spreadsheet import system
-    -terminal for advanced users
-    -bug fixes
-    
+  No data available
     
    
 
 `;
-const update = "6.7.26";
+const update = "28.8.26";
 if (typeof update !== "undefined") { //zorgt dat als update er niet is dat dan niet heel de website crashet
   document.getElementById("update").innerText = update;
 }
